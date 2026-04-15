f9ae9701163541a7a24a8ff41c226730c7d50e62
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-04-15 13:30:20 -0300

    Criação da classe itemPedido

---
+++ src/ItemPedido.java
@@ -0,0 +1,30 @@
+public class ItemPedido {
+
+    private String nomeProduto;
+    private double precoUnitario;
+    private int quantidade;
+
+    public ItemPedido(String nomeProduto, double precoUnitario, int quantidade) {
+        this.nomeProduto = nomeProduto;
+        this.precoUnitario = precoUnitario;
+        this.quantidade = quantidade;
+    }
+
+    public double calcularTotal(){
+        return this.getPrecoUnitario() * this.getQuantidade();
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+
+
